768363059a92420fd436dafbd799781f6b617686
ninemilli_song <ninemilli_song@163.com>
2017-12-29 18:20:56 +0800

    iSay组件调整

--- src/components/i-say/ISay.tsx
+++ src/components/i-say/ISay.tsx
@@ -1,23 +1,87 @@
 import * as React from 'react';
+import './style.scss';
+import { autobind } from 'core-decorators';
+const Button = require('antd/lib/button/button');
 
 const _ = {
     uniqueId: require('lodash/uniqueId')
 }
 
 interface ISayProps {
-    className: string;
+    className?: string;
 }
 
+@autobind
 export default class ISay extends React.Component<ISayProps, any> {
     prefixCls = 'i-say';
 
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            mode: 'fold',  // fold or expand
+        }
+    }
+
     render() {
         const { className } = this.props;
+        const { mode } = this.state;
+
+        const textStyle = mode === 'expand' ? { height: 70 } : null
 
         return (
             <div className={ `${this.prefixCls} ${className}`  }>
-                <textarea name="i-say" id={ _.uniqueId(this.prefixCls) } cols={30} rows={10}></textarea>
+                <div className={`${this.prefixCls}-text`}>
+                    {
+                        this.renderMention()
+                    }
+                    {
+                        this.renderError()
+                    }
+                    <textarea 
+                        name="i-say" 
+                        id={ _.uniqueId(this.prefixCls) } 
+                        rows={1}
+                        placeholder="说点啥吧…"
+                        style= { textStyle }
+                        onFocus={ this.textAreaOnFocus }
+                    />
+                </div>
+                {
+                    mode === 'expand' ? this.renderFooter() : null
+                }
             </div>
         )
+    }
+
+    renderMention() {
+        return (
+            <p className={`highlighter ${this.prefixCls}-mention`}>
+            
+            </p>
+        )
+    }
+
+    renderError() {
+        return (
+            <p className={`highlighter ${this.prefixCls}-error`}>
+
+            </p>
+        )
+    }
+
+    renderFooter() {
+        return (
+            <div className={`${this.prefixCls}-footer`}>
+                <Button>发布</Button>
+            </div>
+        )
+    }
+
+    textAreaOnFocus() {
+        // Expand the textarea
+        this.setState({
+            mode: 'expand'
+        });
     }
 }
--- src/routes/qas/components/Body.tsx
+++ src/routes/qas/components/Body.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import MainSider, { MainSiderProps } from '../../../components/page-frame/MainSider';
+import ISay from '../../../components/i-say/ISay';
 
 interface BodyProps extends MainSiderProps {
 
@@ -7,12 +8,18 @@ interface BodyProps extends MainSiderProps {
 
 export default class Body extends MainSider<BodyProps> {
 
+    prefixCls = 'qas-body'
+
     renderMain() {
-        const { prefixCls } = this.props;
 
         return (
-            <div className={ prefixCls }>
-                QAS Body
+            <div className={ this.prefixCls }>
+                <div className={ `${this.prefixCls}-question-area` }>
+                    <ISay />
+                </div>
+                <div className={ `${this.prefixCls}-question-list` }>
+                    qqq
+                </div>
             </div>
         )
     }
