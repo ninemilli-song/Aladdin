c85d6411758fe23cf95757d14e3bde17c52471fe
ninemilli_song <ninemilli_song@163.com>
2018-07-24 14:12:55 +0800

    删除多余注释

--- server/koa/app-koa.js
+++ server/koa/app-koa.js
@@ -5,30 +5,29 @@ const views = require('koa-views');
 // const co = require('co');
 const convert = require('koa-convert');
 const json = require('koa-json')();
-// const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser')();
-// const koabody = require('koa-body')();
 const logger = require('koa-logger')();
 const routers = require('./routes/index');
-// const users = require('./routes/users');
-// const register = require('./routes/register');
-
-// const util = require('util');
 const errorHandler = require('./middleware/error-handler');
 const jwtVerify = require('./middleware/jwt-verify');
 
 // const mysqlConnection = require('./lib/db');
 
-// middlewares
-// app.use(convert(koabody));
+/**
+ * middlewares
+ */
 app.use(convert(bodyparser));
 app.use(convert(json));
 app.use(convert(logger));
 
-// koa static server for product environment
+/**
+ * koa static server for product environment
+ */
 app.use(require('koa-static')('../dist'));
 
-// view page
+/**
+ * view page
+ */
 app.use(views(`${__dirname}/views`, {
     extension: 'jade'
 }));
@@ -46,7 +45,9 @@ app.use(errorHandler);
  */
 app.use(jwtVerify);
 
-// register router
+/**
+ * register router
+ */
 app.use(routers.routes(), router.allowedMethods());
 
 app.on('error', (err, ctx) => {
